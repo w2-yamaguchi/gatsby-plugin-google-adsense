ae58ef5db039bde6d9ab4308fe3fa3d26777a8c1
w2-yamaguchi <14968984+w2-yamaguchi@users.noreply.github.com>
2021-05-07 21:07:30 +0900

    chore: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'es2021': true,
+  },
+  'extends': [
+    'plugin:react/recommended',
+    'google',
+  ],
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true,
+    },
+    'ecmaVersion': 12,
+  },
+  'plugins': [
+    'react',
+  ],
+  'rules': {
+  },
+};
